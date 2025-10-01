76ae90aa81cfa478b3e0a28a858a9b4bc5744adb
queenxiquec <phamthingocquynh1409@gmail.com>
2025-10-01 15:34:44 -0700

    Update MyBag.h

--- MyBag.h
+++ MyBag.h
@@ -1,11 +1,11 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
 class MyBag
 {
 private:
-	int* data;      
-    int size;       
+    int* data;
+    int size;
     int capacity;
 public:
 
@@ -25,18 +25,25 @@ public:
         return size;
     }
 
-    int getCapacity() const 
-    { 
-        return capacity; 
+    int getCapacity() const
+    {
+        return capacity;
+    }
+    
+    bool isEmpty() const
+    {
+        return size == 0;
     }
 
     // resize to new capacity
-    void resize(int newCap) {
- 
+    void resize(int newCap) 
+    {
+
         int* newData = new int[newCap];
 
         // copy old values
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++)
+        {
             newData[i] = data[i];
         }
 
@@ -45,18 +52,22 @@ public:
         capacity = newCap;
     }
 
-    void insert(int value) {
-        if (size == capacity) {
-            resize(capacity + 1); 
+    void insert(int value)
+    {
+        if (size == capacity)
+        {
+            resize(capacity + 1);
         }
 
         data[size] = value;
         size++;
     }
 
-    void search(int value) {
+    void search(int value)
+    {
         bool found = false;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++)
+        {
             if (data[i] == value) {
                 cout << "\n\tValue " << value << " found at index " << i;
                 found = true;
@@ -68,16 +79,63 @@ public:
         }
     }
 
+    void remove(int index)
+    {
+        if (index < 0 || index >= size) 
+        {
+            cout << "\n\tInvalid index. Please enter an index between 0 and " << size - 1 << ".";
+            return;
+        }
 
+        int deletedValue = data[index];
 
-    friend ostream& operator<<(ostream& out, const MyBag& bag) {
+        // shift elements left
+        for (int i = index; i < size - 1; i++)
+        {
+            data[i] = data[i + 1];
+           
+        }
+
+        cout << "\n\t\tValue " << deletedValue << " has been deleted from MyBag.";
+
+        size--;
+        
+    }
+
+    void clear()
+    {
+        delete[] data;
+        capacity = 0;
+        size = 0;
+        data = new int[capacity];
+    }
+
+    void sortAscending()
+    {
+        for (int i = 0; i < size - 1; i++)
+        {
+            for (int j = 0; j < size - i - 1; j++)
+            {
+                if (data[j] > data[j + 1])
+                {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+        cout << "\n\t\tMybag has been sorted.";
+    }
+
+    friend ostream& operator<<(ostream& out, const MyBag& bag)
+    {
         if (bag.size == 0)
         {
-            out << "\n\tMyBag is empty.";
+            out << "\n\t\tMyBag is empty.";
         }
         else
         {
-            out << "\n\tMybag contains these integers: ";
+            out << "\n\tMybag contains these integers: \n";
             for (int i = 0; i < bag.size; i++) {
                 out << "\n\t[" << i << "] - ";
                 out << bag.data[i];
@@ -87,4 +145,3 @@ public:
         return out;
     }
 };
-
